78d02cc9ba02b40bc4ece558d34af0038a48279f
Kerim Mustafa Demir <kerimmstfdemir@gmail.com>
2022-12-06 19:27:25 +0300

    protected states against page refresh

--- personalizedcontactapp/src/index.js
+++ personalizedcontactapp/src/index.js
@@ -4,14 +4,17 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./redux/app/store";
+import store, { persistor } from "./redux/app/store";
+import { PersistGate } from "redux-persist/integration/react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </BrowserRouter>
 );
--- personalizedcontactapp/src/redux/app/store.jsx
+++ personalizedcontactapp/src/redux/app/store.jsx
@@ -1,13 +1,25 @@
 import { configureStore } from "@reduxjs/toolkit";
 import loginReducer from "../features/loginInfoSlice";
 import registerReducer from "../features/registerSlice"
+import thunk from "redux-thunk"
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import { combineReducers, legacy_createStore as createStore, applyMiddleware } from "redux";
 
-//! CREATING
-const store = configureStore({
-    reducer:{
-        loginInfo: loginReducer,
-        registerInfo: registerReducer
-    }
+const combinedReducers = combineReducers({
+    loginInfo: loginReducer,
+    registerInfo: registerReducer
 })
 
-export default store;
+const persistConfig = {
+    key: 'root',
+    storage,
+  }
+
+const persistedReducer = persistReducer(persistConfig, combinedReducers)
+
+//! CREATING
+const store = createStore(persistedReducer, applyMiddleware(thunk));
+export const persistor = persistStore(store)
+
+export default store
